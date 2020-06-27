77c949c9d930d7aa5cab4bc0a58087ab3ffaf68d
maksimka1709 <66752434+maksimka1709@users.noreply.github.com>
2020-06-27 23:17:41 +0300

    Updates

---
+++ precache-manifest.b0b23e229f9446f9ee53def89d07fc5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f04dee668f5de40fe1d0",
+    "url": "/static/css/main.9d29203b.chunk.css"
+  },
+  {
+    "revision": "f04dee668f5de40fe1d0",
+    "url": "/static/js/main.78dfd739.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2c10edabe68d7c8d428c",
+    "url": "/static/css/2.3b3aca65.chunk.css"
+  },
+  {
+    "revision": "2c10edabe68d7c8d428c",
+    "url": "/static/js/2.4878b81b.chunk.js"
+  },
+  {
+    "revision": "34c478ef3e11df9ca27f1df0029a52d4",
+    "url": "/index.html"
+  }
+];
