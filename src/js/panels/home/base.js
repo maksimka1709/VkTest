b0f18bbeb117a81455f05ce2a1f17481b88d60ef
maksimka1709 <66752434+maksimka1709@users.noreply.github.com>
2020-06-28 15:12:31 +0300

    Update base.js

--- src/js/panels/home/base.js
+++ src/js/panels/home/base.js
@@ -3,9 +3,23 @@ import {connect} from 'react-redux';
 
 import {closePopout, goBack, openModal, openPopout, setPage} from '../../store/router/actions';
 
-import {Div, Panel, Alert, Group, Button, PanelHeader} from "@vkontakte/vkui"
+import {Div, Panel, Alert, Group, Button, PanelHeader, View, Header, Cell} from "@vkontakte/vkui"
+import '@vkontakte/vkui/dist/vkui.css';
 
-import testText from './js/components/modals/HomeTextFAQ.js';
+
+function App () {
+  return (
+    <View activePanel="main">
+      <Panel id="main">
+        <PanelHeader>VKUI</PanelHeader>
+        <Group header={<Header mode="secondary">Items</Header>}>
+          <Cell>Hello</Cell>
+          <Cell>World</Cell>
+        </Group>
+      </Panel>
+    </View>
+  );
+}
 
 class HomePanelBase extends React.Component {
 
@@ -32,7 +46,7 @@ class HomePanelBase extends React.Component {
                         <Button mode="secondary" size="l" stretched={true} onClick={() => this.props.openModal('MODAL_PAGE_BOT_INFO')}>Наша команда</Button>
                     </Div>
                     <Div>
-                        <Button mode="secondary" size="l" stretched={true} onClick={() => this.props.openModal('testText')}>FAQ</Button>
+                        <Button mode="secondary" size="l" stretched={true} onClick={() => App}>FAQ</Button>
                     </Div>
                 </Group>
             </Panel>
