34207a45cd4d75b4fdd3b91cc4a7d1b771de18c4
maksimka1709 <66752434+maksimka1709@users.noreply.github.com>
2020-06-28 13:37:22 +0300

    Update base.js

--- src/js/panels/home/base.js
+++ src/js/panels/home/base.js
@@ -15,50 +15,20 @@ class HomePanelBase extends React.Component {
         this.setState({showImg: true});
     };
 
-    openPopout() {
-        this.props.openPopout(
-            <Alert
-                actions={[{
-                    title: 'Нет',
-                    autoclose: true,
-                    style: 'cancel',
-                }, {
-                    title: 'Да',
-                    autoclose: true,
-                    action: this.showImg
-                }]}
-                onClose={() => this.props.closePopout()}
-            >
-                <h2>Вопрос значит</h2>
-                <p>Вас роняли в детстве?</p>
-            </Alert>
-        );
-    }
 
     render() {
         const {id, setPage, withoutEpic} = this.props;
 
         return (
             <Panel id={id}>
-                <PanelHeader>Examples</PanelHeader>
+                <PanelHeader>ЧЁРНЫЙ GTA-RP</PanelHeader>
                 <Group>
                     <Div>
-                        <Button mode="secondary" size="l" stretched={true} onClick={() => setPage('home', 'groups')}>Список моих
-                            групп</Button>
+                        <Button mode="secondary" size="l" stretched={true} onClick={() => this.props.openModal("MODAL_PAGE_BOTS_LIST")}>Наши сообщества</Button>
                     </Div>
                     <Div>
-                        <Button mode="secondary" size="l" stretched={true} onClick={() => this.openPopout()}>Открыть алерт</Button>
+                        <Button mode="secondary" size="l" stretched={true} onClick={() => this.props.openModal('MODAL_PAGE_BOT_INFO'))}>Наша команда</Button>
                     </Div>
-                    <Div>
-                        <Button mode="secondary" size="l" stretched={true} onClick={() => this.props.openModal("MODAL_PAGE_BOTS_LIST")}>Открыть
-                            модальную страницу</Button>
-                    </Div>
-                    {withoutEpic && <Div>
-                        <Button mode="secondary" size="l" stretched={true} onClick={() => setPage('modal', 'filters')}>Открыть модальное окно</Button>
-                    </Div>}
-                    {this.state.showImg && <Div className="div-center">
-                        <img src="https://vk.com/sticker/1-12676-256" alt="Стикер VK"/>
-                    </Div>}
                 </Group>
             </Panel>
         );
@@ -69,8 +39,6 @@ class HomePanelBase extends React.Component {
 const mapDispatchToProps = {
     setPage,
     goBack,
-    openPopout,
-    closePopout,
     openModal
 };
 
