df9d3d20cb5e6e8dea3a9f9ef851768bcbbc4d12
maksimka1709 <66752434+maksimka1709@users.noreply.github.com>
2020-06-28 14:35:48 +0300

    Add files via upload

---
+++ src/js/components/modals/HomeTextFAQ.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { View, Panel, PanelHeader, Header, Group, Cell } from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
+
+function App () {
+  return (
+    <View activePanel="main">
+      <Panel id="main">
+        <PanelHeader>VKUI</PanelHeader>
+        <Group header={<Header mode="secondary">Items</Header>}>
+          <Cell>Hello</Cell>
+          <Cell>World</Cell>
+        </Group>
+      </Panel>
+    </View>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
